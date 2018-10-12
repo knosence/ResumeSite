765dd2444212ac808218c57958fa9b464d9b8f97
Kno Sence <knosence@linux-xibd.local>
2018-10-12 14:44:37 -0400

    Added a new header picture of Atlanta

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
 // import Portfolio from './components/portfolio/portfolio';
-import Testimonials from  './components/testimonials/testimonials';
+//import Testimonials from  './components/testimonials/testimonials';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
 import resumeData from './resumeData';
@@ -15,7 +15,7 @@ class App extends Component {
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
         {/* <Portfolio resumeData={resumeData}/> */}
-        <Testimonials resumeData={resumeData}/>
+        {/*<Testimonials resumeData={resumeData}/>*/}
         <ContactUs resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
       </div>
